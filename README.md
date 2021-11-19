9ee1e8b9c525de301cfe49d67146452938ba5fb2
tomahawk-jupiter <brianbrainrogers@gmail.com>
2021-11-19 18:17:48 +0000

    Off/on button and keyboard inputs

--- README.md
+++ README.md
@@ -3,17 +3,7 @@ A basic calculator project.
 
 ### Things to improve:
 
-1. The Back, CE, C buttons all just act as reset at the moment.
-
-  a. CE should clear digits just entered.
-
-  b. Back should clear just one digit.
-
-2. I might change one of these buttons into an on/off instead.
-
-3. Include keyboard inputs.
-
-4. Set style dimensions using CSS calc so they reference the container, this will allow everything to be set by changing just one or two values.
+1. Include keyboard inputs.
 
 ### Useful things I learnt
 
--- index.js
+++ index.js
@@ -1,50 +1,65 @@
 
-const numberBtn = document.querySelectorAll('.number-key');
+const numberBtn = document.querySelectorAll('.number-btn');
 const display = document.querySelector('.display');
-const equals = document.querySelector('.equals-key');
-const deleteBtn = document.querySelectorAll('.delete-key');
-const operatorBtn = document.querySelectorAll('.operator-key');
+const equals = document.querySelector('.equals-btn');
+const clearBtn = document.querySelectorAll('.clear-btn');
+const operatorBtn = document.querySelectorAll('.operator-btn');
+const backBtn = document.querySelector('.back-btn');
+const offBtn = document.querySelector('.off-btn');
 
 let firstNumber = null;
 let operator = null;
 let secondNumber = null;
 let equalsPressed = false;
+let off = false;
 
 // Also handles decimal point.
 const handleNumber = (e) => {
-  let input = e.target.innerText;
-  if (!operator){
-    if (firstNumber && firstNumber.includes('.') && input == '.') {
+  let input;
+  if (e.type === 'keydown' && /[0-9]|\./.test(e.key)) {
+    input = e.key;
+  } else if (e.type === 'click') {
+    input = e.target.innerText;
+  } else {
+    input = '';
+  }
+
+  if (!operator && !off && input){
+    if (firstNumber && firstNumber.includes('.') && input === '.') {
       display.innerText = '. error';
     } else {
-      firstNumber = firstNumber === null ? input : firstNumber + input;
+      firstNumber = firstNumber === null && input === '.' ? '0' + '.'
+      : firstNumber === null ? input : firstNumber + input;
       display.innerText = firstNumber;
     }
-  } else if (operator) {
-    if (secondNumber && secondNumber.includes('.') && input == '.') {
+  } else if (operator && !off && input) {
+    if (secondNumber && secondNumber.includes('.') && input === '.') {
       display.innerText = '. error';
     } else {
-      secondNumber = secondNumber === null ? input : secondNumber + input;
+      secondNumber = secondNumber === null && input === '.' ? '0' + '.'
+      : secondNumber === null ? input : secondNumber + input;
       display.innerText = secondNumber;
     }
   }
-  display.style.color = 'black';
+  display.style.color = input ? '#141111' : 'green';
 }
 
 const calculation = () => {
-  let answer = operator == '/' ? +firstNumber / +secondNumber
-      : operator == '*' ? +firstNumber * +secondNumber
-      : operator == '-' ? +firstNumber - +secondNumber
-      : operator == '+' ? +firstNumber + +secondNumber
-      : 'error';
+  if (!off) {
+    let answer = operator === '/' ? +firstNumber / +secondNumber
+        : operator === '*' ? +firstNumber * +secondNumber
+        : operator === '-' ? +firstNumber - +secondNumber
+        : operator === '+' ? +firstNumber + +secondNumber
+        : 'error';
 
-  if (String(answer).length > 9){
-    answer = answer.toFixed(2);
-    if (answer > 9){
-      answer = 'max error';
+    if (String(answer).length > 9){
+      answer = answer.toFixed(2);
+      if (answer > 9){
+        answer = 'max error';
+      }
     }
+    return answer;
   }
-  return answer;
 }
 /*
 The operators act like equals when pressing a second time
@@ -52,41 +67,92 @@ without pressing equals button, example:
 4 + 4 + (display 8) 4 + (display 12)
 */
 const handleOperator = (e) => {
-  let input = e.target.innerText;
-  if (operator && equalsPressed == false){
-    firstNumber = calculation();
-    display.innerText = firstNumber;
+  let input;
+  if (e.type === 'keydown' && /\/|\*|-|\+/.test(e.key)) {
+    input = e.key;
+  } else if (e.type === 'click') {
+    input = e.target.innerText;
+  } else {
+    input = '';
   }
-  operator = input;
-  display.style.color = 'green';
-  secondNumber = null;
+  if (!off && input) {
+    if (operator && equalsPressed == false){
+      firstNumber = calculation();
+      display.innerText = firstNumber;
+    }
+    operator = input;
+    display.style.color = 'green';
+    secondNumber = null;
+    equalsPressed = false;
+  }
 }
 
 const handleEquals = () => {
-  let answer = calculation();
-  display.innerText = answer;
-  firstNumber = answer;
-  equalsPressed = true;
+  if (!off) {
+    let answer = calculation();
+    display.innerText = answer;
+    firstNumber = answer;
+    equalsPressed = true;
+  }
 }
 
-const handleDelete = () => {
+const handleOff = () => {
+  if (off == false) {
+    handleClear();
+    display.style['background-color'] = 'black';
+    display.style.color = '#141111';
+    off = true;
+  } else if (off == true) {
+    display.style['background-color'] = '#c7bcbcfc';
+    off = false;
+  }
+}
+
+const handleClear = () => {
   display.innerText = 0;
   firstNumber = null;
   operator = null;
   secondNumber = null;
   equalsPressed = false;
+  display.style.color = '#141111';
+}
+
+const handleBack = () => {
+  if (!operator && firstNumber){
+    firstNumber = firstNumber.slice(0, -1);
+    display.innerText = firstNumber;
+  } else if (operator && secondNumber) {
+    secondNumber = secondNumber.slice(0, -1);
+    display.innerText = secondNumber;
+  }
 }
 
 numberBtn.forEach(element => {
   element.addEventListener('click', handleNumber);
 });
 
-deleteBtn.forEach(element => {
-  element.addEventListener('click', handleDelete);
+offBtn.addEventListener('click', handleOff);
+
+clearBtn.forEach(element => {
+  element.addEventListener('click', handleClear);
 });
+
+backBtn.addEventListener('click', handleBack);
 
 operatorBtn.forEach(element => {
   element.addEventListener('click', handleOperator);
 });
 
 equals.addEventListener('click', handleEquals);
+
+document.addEventListener('keydown', (e)=> {
+  if (/\/|\*|-|\+/.test(e.key)) {
+    handleOperator(e);
+  }
+  if (/\d|\./.test(e.key)) {
+    handleNumber(e);
+  }
+  if (e.key === 'Enter') {
+    handleEquals();
+  }
+});
